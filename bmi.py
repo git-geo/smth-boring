7aa2bcdc4ca665d193f402cb65e6ca379196009c
git-geo <reallywilliamdavis@gmail.com>
2022-08-29 13:13:18 +0300

    I replaced all IMB words with BMI

--- bmi.py
+++ bmi.py
@@ -1,11 +1,11 @@
 weight = float(input("Your weight: "))
 height = float(input("Your height: "))
 
-ibm = weight / height ** 2
+BMI = weight / height ** 2
 
-if 18.5 <= ibm <= 25:
+if 18.5 <= BMI <= 25:
     print("Your weight is ok")
-elif ibm < 18.5:
+elif BMI < 18.5:
     print("You should gain weight")
 else:
     print("You should lose weight")
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # smth-boring
 This is my first repo created by Git!
-I just created a simple IMB program
+I just created a simple BMI program
